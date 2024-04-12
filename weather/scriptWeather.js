9a165fd314d6689d3fdb59b77d7152b665f9c927
nyxrotten <nes_calina@hotmail.com>
2024-04-12 20:51:13 +0200

    git pages

--- weather/scriptWeather.js
+++ weather/scriptWeather.js
@@ -77,36 +77,35 @@ const getWeatherThisWeek = async () => {
 
 getWeatherThisWeek();
 
-// const getWeatherHours = async () => {
-//     try {
-//         const response = await fetch (forecastBase+apiKey+ciudad+forecastHours);
-//         if (!response.ok) {
-//             throw new Error("Error de API", response.status);
-//         } 
-//         const data = await response.json();
-//         const forecastDayHours = data.forecast.forecastday[0].hour
-//         const hours = document.querySelector('.hours');
-//         console.log(hours)
+const getWeatherHours = async () => {
+    try {
+        const response = await fetch (forecastBase+apiKey+ciudad+forecastHours);
+        if (!response.ok) {
+            throw new Error("Error de API", response.status);
+        } 
+        const data = await response.json();
+        const forecastDayHours = data.forecast.forecastday[0].hour
+        const hours = document.querySelector('.hours');
+        console.log(hours)
         
-//         forecastDayHours.forEach((hour) => {
-//             const liDom = document.createElement('li');
-//             liDom.innerHTML = `
-//                         <img src="${hour.condition.icon}">
-//                         <p>${hour.condition.text}</p>
-//                         <p>${hour.temp_c}</p>
-//                         <p>${hour.wind_kph} ${hour.wind_dir}</p>`
-//                         hours.appendChild(liDom);
+        forecastDayHours.forEach((hour) => {
+            const liDom = document.createElement('li');
+            liDom.innerHTML = `
+                        <img src="${hour.condition.icon}">
+                        <p>${hour.condition.text}</p>
+                        <p>${hour.temp_c}</p>
+                        <p>${hour.wind_kph} ${hour.wind_dir}</p>`
+                        hours.appendChild(liDom);
 
-//         })
+        })
 
         
-//     }
-//     catch (error) {
-//         console.log('Error', error)
-//     }
+    }
+    catch (error) {
+        console.log('Error', error)
+    }
 
-// };
+};
 
-// getWeatherHours();
-
+getWeatherHours();
 
